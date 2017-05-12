6b2e977cd458ecba3917f81500cfc984a6ef672c
byaldira <burak_yaldirak@hotmail.com>
2017-05-12 19:20:12 +0300

    CreateDepartment and Create Faculty
    CreateDepartment and Create Faculty

--- uni-eventor-admin/src/component/CreateDepartment.jsx
+++ uni-eventor-admin/src/component/CreateDepartment.jsx
@@ -9,10 +9,47 @@ class CreateDepartment extends Component {
     }
 
     render() {
-       
+          var resizenone={
+           resize : 'none'
+       }
        return (
            <div>
-             <h2>Fakülte Tanımlama</h2>
+             <h2>Bölüm Tanımlama</h2>
+             <div className="w3-row w3-section">
+                <div className="w3-half w3-container">
+                    <select className="w3-select w3-border w3-padding" name="option">
+                        <option value="" disabled selected>Üniversite Seç</option>
+                        <option value="SAU54">SAU - Sakarya Üniversitesi</option>
+                        <option value="KOU41">KOU - Kocaeli Üniversitesi</option>
+                    </select>
+                </div>
+            </div>
+            <div className="w3-row w3-section">
+                <div className="w3-container w3-half">
+                    <select className="w3-select w3-border w3-padding" name="option">
+                        <option value="" disabled selected>Fakülte Seç</option>
+                        <option value="SAU54">BBF - Bilgisayar ve Bilişim Bilimleri Fakültesi</option>
+                        <option value="KOU41">MF - Mühendislik Fakültesi</option>
+                    </select>
+                </div>
+            </div>
+            <div className="w3-row w3-section">
+                <div className="w3-container w3-half">
+                    <input className="w3-input w3-border w3-padding" type="text" placeholder="Bölüm Adı" id="txtUniName"/>
+                </div>
+            </div>
+            <div className="w3-row w3-section">
+                <div className="w3-container w3-half">
+                        <textarea className="w3-input w3-border w3-padding resizenone"  placeholder="Bölüm Adres"></textarea>
+                </div>
+            </div>
+            <div className="w3-row ">
+                <div className="w3-half">
+                    <div className="w3-container w3-right">
+                        <button className="w3-btn w3-ripple w3-green">Bölümü Ekle</button>
+                    </div>
+                </div>
+            </div>
            </div>
        );
     }
--- uni-eventor-admin/src/component/CreateFaculty.jsx
+++ uni-eventor-admin/src/component/CreateFaculty.jsx
@@ -9,10 +9,38 @@ class CreateFaculty extends Component {
     }
 
     render() {
-       
+        var resizenone={
+           resize : 'none'
+       }
        return (
            <div>
              <h2>Fakülte Tanımlama</h2>
+             <div className="w3-row w3-section">
+                <div className="w3-half w3-container">
+                    <select className="w3-select w3-border w3-padding" name="option">
+                        <option value="" disabled selected>Üniversite Seç</option>
+                        <option value="SAU54">SAU - Sakarya Üniversitesi</option>
+                        <option value="KOU41">KOU - Kocaeli Üniversitesi</option>
+                    </select>
+                </div>
+            </div>
+            <div className="w3-row w3-section">
+                <div className="w3-container w3-half">
+                    <input className="w3-input w3-border w3-padding" type="text" placeholder="Fakülte Adı" id="txtUniName"/>
+                </div>
+            </div>
+            <div className="w3-row w3-section">
+                <div className="w3-container w3-half">
+                        <textarea className="w3-input w3-border w3-padding resizenone" placeholder="Fakülte Adres"></textarea>
+                </div>
+            </div>
+            <div className="w3-row ">
+                <div className="w3-half">
+                    <div className="w3-container w3-right">
+                        <button className="w3-btn w3-ripple w3-green">Fakülteyi Ekle</button>
+                    </div>
+                </div>
+            </div>
            </div>
        );
     }
